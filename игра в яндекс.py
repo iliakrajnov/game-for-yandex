c18934bc3cea9b13f6ef04623affd75026bebc86
LokirTreg <57491076+LokirTreg@users.noreply.github.com>
2019-12-22 06:07:54 +0400

    Add files via upload

--- игра в яндекс.py
+++ игра в яндекс.py
@@ -1,6 +1,79 @@
 import pygame
 import cv2
+import sys
+import os
+import random
 from PIL import Image
+
+
+FPS = 50
+all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    return image
+
+def start_screen():
+    fon = pygame.transform.scale(load_image('logo.png'), (1280, 600))
+    screen.blit(fon, (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+
+def start_screen2():
+    intro_text = ["Управление интуетивное",
+                  "Нажмите на любую клавишу, чтобы продолжить..."]
+    screen.fill((0, 0, 0))
+
+    title = pygame.font.Font(None, 120).render('PONG', 1, (255, 0, 0))
+    screen.blit(title, ((width - title.get_rect().width) // 2, 200))
+
+    font = pygame.font.Font(None, 30)
+    text_coord = 200 + title.get_rect().bottom
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = (width - string_rendered.get_rect().width) // 2
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    color = [255, 0, 0]
+    cycle = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        if cycle == 0:
+            color[0] -= 1
+            color[1] += 1
+            if color == [0, 255, 0]:
+                cycle = 1
+        elif cycle == 1:
+            color[1] -= 1
+            color[2] += 1
+            if color == [0, 0, 255]:
+                cycle = 2
+        elif cycle == 2:
+            color[2] -= 1
+            color[0] += 1
+            if color == [255, 0, 0]:
+                cycle = 0
+        title = pygame.font.Font(None, 120).render('PONG', 1, color)
+        screen.blit(title, ((width - title.get_rect().width) // 2, 200))
+
+        pygame.display.flip()
+        clock.tick(FPS)
 
 class ball:
     def __init__(self):
@@ -12,8 +85,8 @@ class ball:
         sprite = pygame.sprite.Sprite()
         sprite.image = pygame.image.load("ball.png")
         sprite.rect = sprite.image.get_rect()
-        sprite.rect.x = 500
-        sprite.rect.y = 10
+        sprite.rect.x = random.randint(1,1200)
+        sprite.rect.y = random.randint(1, 40)
         self.all_balls.add(sprite)
         self.data.append([12, 12, 0])
         
@@ -42,7 +115,7 @@ class ball:
                             break
             else:
                 self.data[counter][2] -= 1
-            if int(y) <= 0 or int(y) >= 960:
+            if int(y) <= 0 or int(y) >= 600:
                 self.data[counter][1] = -self.data[counter][1]
                 self.sound1.play()
             if int(x) <= 0 or int(x) >= 1280:
@@ -54,16 +127,63 @@ class ball:
         self.all_balls.draw(screen)
 
 
+class bomb(ball):
+    def new_bomb(self):
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.image.load("bomb.png")
+        sprite.rect = sprite.image.get_rect()
+        sprite.rect.x = random.randint(1,1200)
+        sprite.rect.y = random.randint(1, 40)
+        self.all_balls.add(sprite)
+        self.data.append([12, 12, 0])
 
-class game(ball):
+    # def move(self):
+    #     counter = 0
+    #     for sprite in self.all_balls:
+    #         x, y = sprite.rect.x, sprite.rect.y
+    #         if self.data[counter][2] == 0:
+    #             for i in self.mot:
+    #                 if sprite.rect.collidepoint(i):
+    #                     self.data[counter][2] = 5
+    #                     horisontal = 0
+    #                     vertical = 0
+    #                     for j in self.mot:
+    #                         if j[0] in range(i[0] - 10, i[0] + 10):
+    #                             horisontal += 1
+    #                         if j[1] in range(i[1] - 10, i[1] + 10):
+    #                             vertical += 1
+    #                     if horisontal > vertical:
+    #                         self.data[counter][0] = -self.data[counter][0]
+    #                         self.sound1.play()
+    #                         break
+    #                     elif vertical > horisontal:
+    #                         self.data[counter][1] = -self.data[counter][1]
+    #                         self.sound1.play()
+    #                         break
+    #         else:
+    #             self.data[counter][2] -= 1
+    #         if int(y) <= 0 or int(y) >= 600:
+    #             self.data[counter][1] = -self.data[counter][1]
+    #             self.sound1.play()
+    #         if int(x) <= 0 or int(x) >= 1280:
+    #             self.data[counter][0] = -self.data[counter][0]
+    #             self.sound1.play()
+    #         sprite.rect.y += self.data[counter][1]
+    #         sprite.rect.x += self.data[counter][0]
+    #         counter += 1
+    #     self.all_balls.draw(screen)
+
+class game(bomb):
     def __init__(self, old):
         super().__init__()
         self.old = old
         self.mot = []
 
+
+
         
     def work(self, img):
-        self.img = img.resize((1280, 960))
+        self.img = img.resize((1280, 600))
         xs, ys = self.img.size
         pix = self.img.load()
         self.mot = []
@@ -82,18 +202,23 @@ class game(ball):
     def draw(self, img):
         self.work(img)
         img = self.img.tobytes()
-        img = pygame.image.fromstring(img, (1280, 960), 'RGB')
+        img = pygame.image.fromstring(img, (1280, 600), 'RGB')
         screen.blit(img, (0,0))
         self.move()
 
 
 pygame.init()
-size = width, height = 1280, 960
+size = width, height = 1280, 600
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+start_screen()
+start_screen2()
 running = True
+
 cap = cv2.VideoCapture(0)
 g = game([])
 g.new_ball()
+g.new_bomb()
 while running:
     _, frame = cap.read()
     frame = cv2.flip(frame,1)
@@ -101,4 +226,4 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    g.draw(Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)))
+    g.draw(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
